c675291a621c216a1920ca5982a77db4ff8c0f02
egemensert <egemensert@gmail.com>
2023-05-27 15:49:52 +0300

    Version 1

--- train_utils.py
+++ train_utils.py
@@ -58,6 +58,33 @@ def ablation(generator, fixed_z, device, alpha, discriminator_step, output_dir, 
 
     save_image(gen_imgs[:fix_num], os.path.join(output_dir, f"{discriminator_step}_{suffix}.png"), nrow=fix_row, normalize=True)
 
+def ablation_cpu(generator, fixed_z, device, alpha, discriminator_step, output_dir, metadata, suffix, h_stddev, h_mean=None, psi=None, fix_row=5, fix_num=20, random=False):
+    with torch.no_grad():
+        copied_metadata = copy.deepcopy(metadata)
+        copied_metadata['h_stddev'] = copied_metadata['v_stddev'] = h_stddev
+        if h_mean != None:
+            copied_metadata['h_mean'] += h_mean
+        
+        if psi != None:
+            copied_metadata['psi'] = psi
+        gen_imgs = []
+        arg = None
+        if random:
+            rand_z = torch.randn_like(fixed_z)
+            arg = rand_z
+        else:
+            arg = fixed_z
+
+        for idx in range(fixed_z.shape[0]):
+            g_imgs, _, _, _ = generator(arg[idx:idx+1].to(device), alpha=alpha, **copied_metadata)
+            gen_imgs.append(g_imgs)
+        
+        gen_imgs = torch.cat(gen_imgs, axis=0)
+        gen_imgs = ((gen_imgs + 1) / 2).float()
+        gen_imgs = gen_imgs.clamp_(0, 1)
+
+    save_image(gen_imgs[:fix_num], os.path.join(output_dir, f"{discriminator_step}_{suffix}.png"), nrow=fix_row, normalize=True)
+
 class CelebAHQ(Dataset):
     """CelelebA Dataset"""
 
